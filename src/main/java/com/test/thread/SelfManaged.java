92567309ffa0410a71826dc1ac8cacf4ac50fb89
chc810 <chc810@163.com>
2015-01-19 11:55:13 +0800

    增加junit

--- src/main/java/com/test/thread/SelfManaged.java
+++ src/main/java/com/test/thread/SelfManaged.java
@@ -1,5 +1,7 @@
 package com.test.thread;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * 自管理的线程写法，可以不让线程实现Thread类
  * @author cuihc
@@ -29,6 +31,12 @@ public class SelfManaged implements Runnable{
 	}
 	
 	public static void main(String[] args) {
+		try {
+			TimeUnit.MILLISECONDS.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		for (int i=0; i<5;i++) {
 			new SelfManaged();
 		}
